db9e6843a4a7e95e47ec965fd6c21f60a2fee1b9
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2026-02-15 23:36:56 +0200

    init xcode

---
+++ Dsquares-task/Dsquares-task/Dsquares_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dsquares_taskApp.swift
+//  Dsquares-task
+//
+//  Created by Mohamed Salah on 15/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Dsquares_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dsquares-task/Dsquares-taskTests/Dsquares_taskTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dsquares_taskTests.swift
+//  Dsquares-taskTests
+//
+//  Created by Mohamed Salah on 15/02/2026.
+//
+
+import Testing
+@testable import Dsquares_task
+
+struct Dsquares_taskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
